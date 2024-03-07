19097e8524e96252a093b181c715082b4e0734be
unknown <ashishnagpal2498@gmail.com>
2024-03-07 18:23:11 -0400

    Backend - pre-check AWS credentials

---
+++ backend/src/main/java/com/cloud/imagicon/precheck/AWSCredentialsChecker.java
@@ -0,0 +1,42 @@
+package com.cloud.imagicon.precheck;
+
+import com.amazonaws.auth.BasicSessionCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Component
+public class AWSCredentialsChecker implements ApplicationRunner {
+
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.sessionToken}")
+    private String sessionToken;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // Perform the AWS credentials validation here
+        try {
+            BasicSessionCredentials awsCredentials = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                    .build();
+
+            s3Client.listBuckets();
+
+            System.out.println("AWS credentials validation successful.");
+
+        } catch (Exception e) {
+            System.out.println("AWS credentials invalid/ expired");
+        }
+    }
+}
