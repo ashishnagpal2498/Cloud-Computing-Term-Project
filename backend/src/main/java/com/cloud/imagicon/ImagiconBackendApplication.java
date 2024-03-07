e0c66210af05b00cd0fa5aa220652e2cc89c1043
unknown <ashishnagpal2498@gmail.com>
2024-03-07 18:20:47 -0400

    Backend - configuration

--- backend/src/main/java/com/cloud/imagicon/ImagiconBackendApplication.java
+++ backend/src/main/java/com/cloud/imagicon/ImagiconBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ImagiconBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ImagiconBackendApplication.class, args);
 	}
 
--- backend/src/test/java/com/cloud/imagicon/ImagiconBackendApplicationTests.java
+++ backend/src/test/java/com/cloud/imagicon/ImagiconBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PostcardBackendApplicationTests {
+class ImagiconBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
